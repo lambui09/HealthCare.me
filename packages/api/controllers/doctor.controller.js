e5bcfb3f441e2d6a82e7c60d5a7cd95254238f49
lambui09 <buiduclam09@gmail.com>
2020-04-13 20:30:38 +0700

    add favorite to doctor

--- packages/api/controllers/doctor.controller.js
+++ packages/api/controllers/doctor.controller.js
@@ -1,4 +1,5 @@
 const Doctor = require('../models/Doctor');
+const Favorite = require('../models/Favorite');
 
 const createDoctor = async (req, res) => {
 
@@ -7,23 +8,23 @@ const createDoctor = async (req, res) => {
 const getAllDoctor = async (req, res) => {
     const page = +req.query.page || 1;
     const page_size = 12;
-    const skip = page_size * (page -1);
+    const skip = page_size * (page - 1);
     const limit = page_size;
     let doctors = [];
-    try{
+    try {
         doctors = await Doctor.find().skip(skip).limit(limit)
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         doctors = []
     }
     let total_users = [];
     try {
         total_users = await Doctor.countDocuments()
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         total_users = []
     }
-    const total_page = Math.ceil(total_users /page_size);
+    const total_page = Math.ceil(total_users / page_size);
     return res.status(200).json({
         success: true,
         data: {
@@ -41,13 +42,13 @@ const getDetailDoctor = async (req, res) => {
     const errors = {};
     const {doctorId} = req.params;
     let doctor = null;
-    try{
+    try {
         doctor = await Doctor.findById(doctorId);
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         doctor = null;
     }
-    if (!doctor){
+    if (!doctor) {
         errors.error = 'Can\'t get detail doctor, Please try again later';
         return res.status(404).json(
             {
@@ -58,12 +59,100 @@ const getDetailDoctor = async (req, res) => {
     }
     return res.status(200).json({
         success: true,
-        data:{
+        data: {
             doctor,
         },
     });
 };
+
+const addFavorite = async (req, res) => {
+    const {
+        user
+    } = req;
+    const errors = {};
+    const {doctorId} = req.params;
+    console.log(doctorId)
+    let doctor = null;
+    try {
+        doctor = await Doctor.findById(doctorId);
+    } catch (error) {
+        console.log(error);
+        doctor = null;
+    }
+    if (!doctor) {
+        errors.error = 'Doctor not found!';
+        return res.status(500).json({
+            success: false,
+            errors,
+        });
+    }
+
+    let favorite = null;
+    try {
+        favorite = await Favorite.findOne({
+            doctor: doctorId,
+        })
+    } catch (error) {
+        console.log(error);
+        favorite = null;
+    }
+
+    if (!favorite) {
+        const data = {
+            favorite_person: user,
+            doctor: doctorId,
+        };
+    }
+
+    console.log(data)
+    const newDoctorFavorite = new Favorite(data);
+    let doctorFavoriteCreated = null;
+    try {
+        doctorFavoriteCreated = await newDoctorFavorite.save(data)
+    } catch (error) {
+        console.log(error);
+        doctorFavoriteCreated = null;
+    }
+    if (!doctorFavoriteCreated) {
+        errors.error = 'Can\'t not create favorite for doctor!';
+        return res.status(400).json({
+            success: false,
+            errors,
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: {
+            doctorFavoriteCreated,
+        },
+    });
+    //clear add to favorite
+    let doctorFavoriteDeleted = null;
+    try {
+        doctorFavoriteDeleted = await Favorite.findOneAndDelete({
+            doctor: doctorId,
+        })
+    } catch (error) {
+        console.log(error);
+        doctorFavoriteDeleted = null;
+    }
+
+    if (!doctorFavoriteDeleted) {
+        errors.error = 'Can\'t not remove favorite for doctor!';
+        return res.status(400).json({
+            success: false,
+            errors,
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: {
+            doctorFavoriteDeleted,
+        },
+    });
+}
 module.exports = {
-  getAllDoctor,
-  getDetailDoctor,
+    getAllDoctor,
+    getDetailDoctor,
+    addFavorite,
 };
--- packages/api/routes/doctor.js
+++ packages/api/routes/doctor.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const doctorController = require('/controllers/doctor.controller');
+const doctorController = require('../controllers/doctor.controller');
+const passport = require('passport');
 const router = express.Router();
-router.get('/', doctorController.getAllDoctor());
-router.get('/:doctorId',doctorController.getDetailDoctor());
+router.get('/', doctorController.getAllDoctor);
+router.get('/:doctorId',doctorController.getDetailDoctor);
+router.get('/:doctorId/favorite',passport.authenticate('jwt', {
+    session: false
+}),doctorController.addFavorite)
 module.exports = router;
