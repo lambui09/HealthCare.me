59af88e744c2b5920ba6ea1baa858a45c97da5a0
An Ng. Quang <ngquangan@gmail.com>
2020-04-13 23:17:45 +0700

    Update config db

--- packages/api/config/database/index.js
+++ packages/api/config/database/index.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    }).then(() => {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+}).then(() => {
+    mongoose.connection.collection('doctors', (err, collection) => {
+        if (!err) {
+            collection.createIndex({
+                location: "2dsphere"
+            })
+        }
+    });
+    mongoose.connection.collection('patients', (err, collection) => {
+        if (!err) {
+            collection.createIndex({
+                location: "2dsphere"
+            })
+        }
+    });
     console.log('MongoDB connected...');
 });
