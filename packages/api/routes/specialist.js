a81d4e99a9ebe826639e99cccc1315453bae25f9
lambui09 <buiduclam09@gmail.com>
2020-04-20 16:58:05 +0700

    update create specialist

---
+++ packages/api/routes/specialist.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const SpecialistController = require('../controllers/specialist.controller');
+const passport = require('passport');
+const router = express.Router();
+router.post('',);
+router.
+module.exports = router;
