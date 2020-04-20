3503f66a485d179f1683c32b1238752813852799
lambui09 <buiduclam09@gmail.com>
2020-04-21 01:23:25 +0700

    update create symptom

--- packages/api/routes/surveySymptom.js
+++ packages/api/routes/surveySymptom.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const surveySymptom = require('../controllers/survey_symptom.controller');
+const passport = require('passport');
 const router = express.Router();
 router.post('/', surveySymptom.createSymptom);
+router.patch('/:symptom_id', passport.authenticate('jwt', {
+    session: false
+}), surveySymptom.updateSymptom);
+router.delete('/:symptom_id', passport.authenticate('jwt', {
+    session: false
+}), surveySymptom.deleteSymptom);
 module.exports = router;
