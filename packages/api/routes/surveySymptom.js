59bd14ac55c3a0773d5eaa6305db171fd5262aae
lambui09 <buiduclam09@gmail.com>
2020-04-21 01:25:14 +0700

    fix confict

--- packages/api/routes/surveySymptom.js
+++ packages/api/routes/surveySymptom.js
@@ -3,10 +3,6 @@ const surveySymptom = require('../controllers/survey_symptom.controller');
 const passport = require('passport');
 const router = express.Router();
 router.post('/', surveySymptom.createSymptom);
-router.patch('/:symptom_id', passport.authenticate('jwt', {
-    session: false
-}), surveySymptom.updateSymptom);
-router.delete('/:symptom_id', passport.authenticate('jwt', {
-    session: false
-}), surveySymptom.deleteSymptom);
+router.patch('/:symptom_id', passport.authenticate('jwt', {session: false}), surveySymptom.updateSymptom);
+router.delete('/:symptom_id', passport.authenticate('jwt', {session: false}), surveySymptom.deleteSymptom);
 module.exports = router;
