0f6c4d9f6848d29355733e4bc579b23f2dcca299
lambui09 <buiduclam09@gmail.com>
2020-05-08 14:49:47 +0700

    change json

--- packages/main/server.js
+++ packages/main/server.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./healthy-care-9834a-firebase-adminsdk-yco1r-634413df99');
+const serviceAccount = require('./health-care-276516-firebase-adminsdk-c2cwl-09c7c12f53');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://healthy-care-9834a.firebaseio.com"
+    databaseURL: "https://health-care-276516.firebaseio.com"
 });
 module.exports.admin = admin;
