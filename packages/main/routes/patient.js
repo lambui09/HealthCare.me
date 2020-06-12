deb1a3fa0ee40971a0015700abb331c2ce794db2
Bui Duc Lam <buiduclam09@gmail.com>
2020-06-12 16:21:21 +0700

    update endpoint get infor user

--- packages/main/routes/patient.js
+++ packages/main/routes/patient.js
@@ -2,4 +2,5 @@ const express = require('express');
 const patientController = require('../controllers/patient.controller');
 const router = express.Router();
 router.patch('/:patient_id', patientController.updatePatient);
+router.get('/:patient_id', patientController.getDetailPatient);
 module.exports = router;
